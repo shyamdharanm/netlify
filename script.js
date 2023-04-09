ebfa71c981791cafc3235110242d402b7b3ae437
shyamdharanm <shyamdharan214@gmail.com>
2023-04-09 12:27:22 +0530

    s

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-var sam=document.createElement("div");
-sam.innerHTML(date and year);
-document.body.append(sam);
+<script>
+  function target() {
+    var form = document.getElementById('HTMLFORM');
+    var firstname = form.elements['firstname'].value;
+    var middlename = form.elements['middlename'].value;
+    const lastname = form.elements['lastname'].value;
+    var email = form.elements['email'].value;
+    console.log('First Name:', firstname);
+    console.log('Middle Name:', middlename);
+    console.log('Last Name:', lastname);
+    console.log('Email:', email);
+  }
+</script>
